1ecd5681e5a3992bd85cae203594c2398e373da5
maik <maikgrigoleit50@gmail.com>
2019-02-08 18:20:52 +0100

    added updater to cappuchat

---
+++ Chat.Updater/Chat.Updater/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Chat.Updater.ViewModels;
+
+namespace Chat.Updater
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void AppOnStartup(object sender, StartupEventArgs e)
+        {
+            var updaterViewModel = new UpdaterViewModel(e.Args[0]);
+            var window = new MainWindow { DataContext = updaterViewModel };
+
+            window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            window.Show();
+        }
+    }
+}
