c7871031e116af9e29d0055273e3e5693b959e69
Hendrik Roskamp <hendrik.roskamp@orgadata.com>
2019-02-18 16:38:50 +0100

    added savemode behavior

---
+++ Chat.Client/Chat.Client/Converters/BooleanToSaveModeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Chat.Client.Converters
+{
+    public class BooleanToSaveModeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool givenValue = (bool)value;
+            if (givenValue)
+                return "Save mode is on";
+            else
+                return "Save mode is off";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
